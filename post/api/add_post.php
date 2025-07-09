24daa421bd1eeaf21884be1f64488d1cb52e2287
vlastimir8987 <Krylov.Vlastimir@gmail.com>
2025-07-09 16:15:44 +0300

    Update add_post.php

--- post/api/add_post.php
+++ post/api/add_post.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // Разрешить запросы с любого домена
+header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header('Content-Type: application/json');
 $file = '../posts.json';
 
